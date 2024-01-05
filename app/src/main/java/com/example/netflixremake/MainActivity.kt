1deed47bed1f91e52483bd71fa2203e3a9516df4
Hugo Jordan <hugoj8366@gmail.com>
2024-01-05 10:52:53 -0300

    Adicionando recyclerView da MainActivity

--- app/src/main/java/com/example/netflixremake/MainActivity.kt
+++ app/src/main/java/com/example/netflixremake/MainActivity.kt
@@ -3,15 +3,54 @@ package com.example.netflixremake
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("lifeCycles","onCreate")
+
+        recyclerView = findViewById(R.id.rv_main)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MainAdapter()
+
     }
 
+    private inner class MainAdapter() : RecyclerView.Adapter<MainAdapter.MovieViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+            val layout: View = layoutInflater.inflate(R.layout.movie_item, parent, false)
+            return MovieViewHolder(layout)
+        }
+
+        override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+            holder.bind()
+        }
+
+        override fun getItemCount(): Int {
+            return 20
+        }
+
+        private inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind() {
+
+            }
+        }
+
+    }
+
+}
+
+
+    /* Study after
     override fun onStart() {
         super.onStart()
         Log.i("lifeCycles","onStart")
@@ -35,5 +74,4 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy(){
         super.onDestroy()
         Log.i("lifeCycles","onDestroy")
-    }
-}
+    } */
