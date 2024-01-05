b81d94afb46768f006c3315c79f0d271f85140b8
Hugo Jordan <hugoj8366@gmail.com>
2024-01-05 09:36:18 -0300

    Alterando tema

--- app/src/main/java/com/example/netflixremake/MainActivity.kt
+++ app/src/main/java/com/example/netflixremake/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.netflixremake
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
