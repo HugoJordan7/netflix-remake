2a4001e8f2557adbacdb44354d815a1e8d8d72cc
Hugo Jordan <hugoj8366@gmail.com>
2024-01-06 21:21:22 -0300

    Adicionando fundo de filme com sombras

--- app/src/main/java/com/example/netflixremake/MovieActivity.kt
+++ app/src/main/java/com/example/netflixremake/MovieActivity.kt
@@ -1,8 +1,12 @@
 package com.example.netflixremake
 
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +18,12 @@ class MovieActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.arrow_back)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        var shadows: LayerDrawable = ContextCompat.getDrawable(this,R.drawable.shadows) as LayerDrawable
+        var imageShadow: Drawable = ContextCompat.getDrawable(this,R.drawable.movie_4) as Drawable
+        shadows.setDrawableByLayerId(R.id.shadows_img,imageShadow)
+        var movieCover = findViewById<ImageView>(R.id.movie_cover)
+        movieCover.setImageDrawable(shadows)
+
     }
 }
