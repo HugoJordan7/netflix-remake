44b9dd3b2f41594aac5316722095c16cefe8a03f
Hugo Jordan <hugoj8366@gmail.com>
2024-01-10 15:17:14 -0300

    Sincronizando fundos dos filmes com as fotos do arquivo json da url

--- app/src/main/java/com/example/netflixremake/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/netflixremake/adapters/MovieAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.netflixremake.adapters
 
+import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,8 @@ import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 import com.example.netflixremake.R
 import com.example.netflixremake.model.Movie
+import com.example.netflixremake.util.DownloadImageTask
+import com.squareup.picasso.Picasso
 
 class MovieAdapter(
         var list: List<Movie>,
@@ -30,7 +33,17 @@ class MovieAdapter(
     inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(movie: Movie) {
             val image: ImageView = itemView.findViewById(R.id.movie_jpg)
+            // Using the Picasso Library
+            //Picasso.get().load(movie.coverUrl).into(image)
 
+            //Using project file manually
+            DownloadImageTask(
+                object : DownloadImageTask.CallBack{
+                    override fun onResult(bitmap: Bitmap) {
+                        image.setImageBitmap(bitmap)
+                    }
+                }
+            ).execute(movie.coverUrl)
         }
     }
 
---
+++ app/src/main/java/com/example/netflixremake/util/DownloadImageTask.kt
@@ -0,0 +1,58 @@
+package com.example.netflixremake.util
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import com.example.netflixremake.model.Category
+import java.io.IOException
+import java.io.InputStream
+import java.net.URL
+import java.util.concurrent.Executors
+import javax.net.ssl.HttpsURLConnection
+
+class DownloadImageTask(private var callBack: CallBack) {
+
+    interface CallBack{
+        fun onResult(bitmap: Bitmap)
+    }
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val executor = Executors.newSingleThreadExecutor()
+
+    fun execute(urlText: String){
+
+        executor.execute {
+            var urlConnection: HttpsURLConnection? = null
+            var stream: InputStream? = null
+
+            try {
+                urlConnection = URL(urlText).openConnection() as HttpsURLConnection
+                urlConnection.readTimeout = 2000
+                urlConnection.connectTimeout = 2000
+
+                if(urlConnection.responseCode > 400){
+                    throw IOException("Server connection error!")
+                }
+
+                stream = urlConnection.inputStream
+
+                val bitmap = BitmapFactory.decodeStream(stream)
+                handler.post {
+                    callBack.onResult(bitmap)
+                }
+
+            } catch (e: IOException){
+                val message: String = e.message ?: "Unknown error!"
+                Log.e("Error Test",message,e)
+            } finally {
+                stream?.close()
+                urlConnection?.disconnect()
+            }
+        }
+
+
+    }
+
+}
